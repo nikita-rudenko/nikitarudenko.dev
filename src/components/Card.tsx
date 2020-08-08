412e873592da7a0155b62f79f51c375517fca0fd
Nikita Rudenko <n.rud3nko@gmail.com>
2020-08-08 23:02:17 +0300

    refactor: :recycle: Make title and href prop optional for Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,8 +3,8 @@ import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
-  title: string
-  href: string
+  title?: string
+  href?: string
   className?: string
 }
 
@@ -15,13 +15,15 @@ const Card = (props: Props) => {
     <div
       className={`p-4 md:p-6 bg-white rounded-md shadow-sm text-mononchrome-800 ${className}`}
     >
-      <h3 className="px-2 py-2 text-xl font-bold md:px-3 md:text-2xl font-body">
-        <Link href={href}>
-          <a className="px-1 transition duration-300 border-b-4 border-yellow-400 border-opacity-75 hover:bg-yellow-400 hover:border-transparent hover:bg-opacity-75">
-            {title}
-          </a>
-        </Link>
-      </h3>
+      {title && href && (
+        <h3 className="px-2 py-2 text-xl font-bold md:px-3 md:text-2xl font-body">
+          <Link href={href}>
+            <a className="px-1 transition duration-300 border-b-4 border-yellow-400 border-opacity-75 hover:bg-yellow-400 hover:border-transparent hover:bg-opacity-75">
+              {title}
+            </a>
+          </Link>
+        </h3>
+      )}
 
       {children}
     </div>

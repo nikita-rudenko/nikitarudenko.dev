10e6e025925d3a95b2a1c23f26de0ea680d04536
Nikita Rudenko <n.rud3nko@gmail.com>
2020-08-08 22:42:35 +0300

    feat: :art: Add Header and reformat layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,40 @@
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
+import links from '@constants/links'
+
 type Props = {
-  children: ReactNode
-  className?: string
+  children?: ReactNode
+  [x: string]: any
 }
 
-const Layout = ({ children }: Props) => <div>{children}</div>
-
-Layout.Header = ({ children }: Props) => <header>{children}</header>
-
-Layout.Body = ({ children, className }: Props) => (
-  <main className={className}>{children}</main>
+const Layout = ({ children, ...props }: Props) => (
+  <div {...props}>{children}</div>
 )
 
-Layout.Footer = () => (
-  <footer>
-    <div className="flex flex-col justify-center px-4 py-4 sm:flex-row sm:items-center sm:justify-evenly md:py-8 md:px-16 font-body text-mononchrome-800">
+Layout.Header = ({ ...props }: Props) => (
+  <header {...props}>
+    <ul className="flex px-2 py-4 text-sm font-bold bg-white md:py-6 md:px-16 font-body md:text-base text-mononchrome-800">
+      {links.map(({ href, label }) => {
+        return (
+          <Link href={href} key={label}>
+            <a className="p-2 mr-4 rounded-md hover:bg-mononchrome-100">
+              <li>{label}</li>
+            </a>
+          </Link>
+        )
+      })}
+    </ul>
+  </header>
+)
+
+Layout.Body = ({ children, ...props }: Props) => (
+  <main {...props}>{children}</main>
+)
+
+Layout.Footer = ({ ...props }: Props) => (
+  <footer {...props}>
+    <div className="flex flex-col justify-center px-4 py-4 bg-white sm:flex-row sm:items-center sm:justify-evenly md:py-8 md:px-16 font-body text-mononchrome-800">
       <div className="text-sm md:text-base">
         Made using{' '}
         <FooterLink className="hover:bg-black" href="https://nextjs.org/">
---
+++ src/constants/links.ts
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Blog',
+    href: '/blog',
+  },
+  {
+    label: 'Projects',
+    href: '/projects',
+  },
+  {
+    label: 'Snippets',
+    href: '/',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+]
+
+export default links

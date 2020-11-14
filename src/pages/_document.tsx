0a35dddccf28332f80ca67fec0424904373c18af
Nikita Rudenko <n.rud3nko@gmail.com>
2020-11-14 14:12:04 +0200

    Disable Splitbee in dev environment and branch deployments Fixes #52

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,14 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 
 class Document extends NextDocument<{ css: string }> {
   render() {
+    const { NODE_ENV, SPLITBEE_ENABLED = '' } = process.env
+
     return (
       <Html lang="en">
         <Head>
-          <script async src="https://cdn.splitbee.io/sb.js"></script>
+          {NODE_ENV === 'production' && JSON.parse(SPLITBEE_ENABLED) && (
+            <script async src="https://cdn.splitbee.io/sb.js" />
+          )}
         </Head>
         <body>
           <script src="/noflash.js" />

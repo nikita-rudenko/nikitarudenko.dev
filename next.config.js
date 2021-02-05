5bde5dafe17db2a6b4f9017e87d51cec05c8bd70
Nikita Rudenko <n.rud3nko@gmail.com>
2021-02-05 17:01:03 +0200

    feat: :zap: Add link rewrite for LinkedIn profile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/nikita-rudenko-291356183/',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = withBundleAnalyzer(nextConfig)
